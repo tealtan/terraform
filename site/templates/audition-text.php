020fff97c058918f589ec0f39927aa2560c0c04f
Allen Tan <allen@tanmade.com>
2014-08-30 16:27:38 -0400

    Added making/audition-text.

---
+++ site/templates/audition-text.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Audition Text demo</title>
+    <meta name="viewport" content="width=device-width">
+    <link href="audition.css" media="screen, projection" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+
+    <div class="shell">
+
+      <h1>Audition Text</h1>
+
+      <p id="output"></p>
+
+      <label>You can test by typing here:</label>
+      <textarea id="input">Very quick test of how one might {audition/toggle between/show you're working through} different variations in a {sentence/line of thought/argument}. I think Charlie's version of showing all the different options is smart, though I think a writer would want to make different choices and see {and try reading it/if the particular set of variations make sense}.</textarea>
+    </div>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="audition.js"></script>​
+
+  </body>
+</html>
