5a3b4a9bd13e24360d3dc759b885f54037abffd1
Allen Tan <allen@tanmade.com>
2014-10-28 06:42:19 -0400

    Added home.

--- site/templates/default.txt.php
+++ site/templates/default.txt.php
@@ -1,5 +1,11 @@
 <?php
 
+// Global links
+echo "Navigate :  http://tanmade.com/navigate";
+echo PHP_EOL;
+echo "Options  :  http://tanmade.com/options";
+echo PHP_EOL . PHP_EOL . PHP_EOL . PHP_EOL;
+
 // Get page uri
 $pathExtension = pathinfo($_SERVER['REQUEST_URI'], PATHINFO_EXTENSION);
 $uri = substr($_SERVER['REQUEST_URI'], 0, -(strlen($pathExtension)+1));
--- site/templates/home.php
+++ site/templates/home.php
@@ -2,7 +2,6 @@
 
   <main class="main" role="main">
 
-    <h1><?php echo html($page->title()) ?></h1>
     <?php echo kirbytext($page->text()) ?>
 
     <hr>
